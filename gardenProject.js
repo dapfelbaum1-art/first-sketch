ec77a96146a8f1e5608117057e62a272f8e1c9f0
dapfelbaum1-art <dapfelbaum1@gmail.com>
2025-09-30 12:29:59 -0500

    Add files via upload

--- gardenProject.js
+++ gardenProject.js
@@ -1,8 +1,10 @@
 
-let furby
+let pink
 let basket
 let garden
 let rose
+let roseCut
+
 
 //empty flower arrays
 let pinkx = []
@@ -16,12 +18,19 @@ let flower1Number = 10
 
 let flower2Number = 20 
 
+let score = 0
+
+let collectFlowersBool = true
+
+let finshedCollectingBool = false
 
 function preload(){
-  furby = loadImage('pinkFlower.png') 
+  pink = loadImage('pinkFlower.png') 
   rose = loadImage ('redRose.png')
   basket = loadImage('basket.png') 
   garden = loadImage ('garden.jpg')
+  rose = loadImage('redRose.png')
+  roseCut = loadImage('redRoseCut.png')
 
 }
 
@@ -32,79 +41,66 @@ function setup(){
 
   //adding multiple numbers to the flower arrays
 
-   for(i = 0; i< flower1Number; i++){
-      pinkx.push(random(windowWidth))
-      pinky.push(random(windowHeight/2, windowHeight))
-   }
+ for(i = 0; i< flower1Number; i++){
+  pinkx.push(random(windowWidth))
+  pinky.push(random(windowHeight/2, windowHeight))
+}
+
+for(i = 0; i< flower2Number; i++){
+  redx.push(random(windowWidth))
+  redy.push(random(windowHeight/2, windowHeight))
+}
+
 
 
 }
 
-// RED
-function setup(){
- createCanvas(windowWidth, windowHeight)
- imageMode(CENTER)
 
-  //adding multiple numbers to the flower arrays
-
-   for(i = 0; i< flower2Number; i++){
-      redx.push(random(windowWidth))
-      redy.push(random(windowHeight/2, windowHeight))
-   }
-
-}
 
 function draw(){
-  background(255)
-image(garden, windowWidth/2, windowHeight/2, windowWidth,windowHeight)
-        
-      //parse through the flower arrays, display flowers
-      //
-
-//PINK
-    
-    for(let i = 0; i < pinkx.length; i++){
-      image(furby, pinkx[i], pinky[i], 50, 50)
-
-      //delete object if your mouse is close
-
-      if(dist(mouseX, mouseY, pinkx[i], pinky[i])< 25){
-        pinkx.splice(i, 1)
-        pinky.splice(i, 1)
-      }
-
-    }
-//RED 
-    for(let i = 0; i < redx.length; i++){
-      image(rose, redx[i], redy[i], 50, 50)
 
 
-    }
 
-    //checking to see if a flower is collected, if so show the flower image in basket
-   if(pinkx.length < flower1Number){
-        image(furby, mouseX - 20, mouseY-10, 50, 50)
-    }
+  if(collectFlowersBool == true){
+    startGame()
+  }
 
-    image(basket, mouseX, mouseY, 150,100)
-    noCursor()
+  if(finshedCollectingBool == true){
+
+    winGame()
+  }
+
+  
 }
+//RED 
+
+
 
 
 
 function mousePressed(){
+  if(collectFlowersBool == true){
   //chakc if mouse is over red
-     for(let i = 0; i < redx.length; i++){
-      if(dist(mouseX,mouseY,redx[i],redy[i])< 25){
-        redx.splice(i, 1)
-        redy.splice(i, 1)
-      }
+ for(let i = 0; i < redx.length; i++){
+  if(dist(mouseX,mouseY,redx[i],redy[i])< 25){
+    redx.splice(i, 1)
+    redy.splice(i, 1)
+    score++ 
+  }
 
+}
+
+for(let i = 0; i < pinkx.length; i++){
+  if(dist(mouseX,mouseY,pinkx[i],pinky[i]) < 25){
+    pinkx.splice(i, 1)
+    pinky.splice(i, 1)
+      score++   // increase score when pink is collected
     }
 
+  }
+  }
+
     //check if mouse is over pink
-
-
 
 }
 
@@ -118,4 +114,80 @@ function keyPressed(){
 
     print(x)
   }
+}
+
+  //function that starts game 
+function startGame(){
+  
+
+  background(255)
+  image(garden, windowWidth/2, windowHeight/2, windowWidth,windowHeight)
+
+  fill (255)
+  textSize (40)
+  text ('Collect all flowers to win!!!' , 10,100)
+  text ('You have ' +score+ ' out of 30', 10, 150)
+
+      //parse through the flower arrays, display flowers
+      //
+  if (score == 30){
+    collectFlowersBool = false
+    finshedCollectingBool =true 
+  }
+//PINK
+
+  for(let i = 0; i < pinkx.length; i++){
+    image(pink, pinkx[i], pinky[i], 50, 50)
+  }
+
+      //delete object if your mouse is close
+      //if(dist(mouseX, mouseY, pinkx[i], pinky[i])< 25){
+    // pinkx.splice(i, 1)
+    // pinky.splice(i, 1)
+  
+
+  for(let i = 0; i < redx.length; i++){
+    image(rose, redx[i], redy[i], 50, 50)
+  }
+
+
+  
+
+  
+
+
+
+
+
+
+    //checking to see if a flower is collected, if so show the flower image in basket
+   // if(pinkx.length < flower1Number){
+   //      image(pink, mouseX - 20, mouseY-10, 50, 50)
+   //  }
+
+  image(basket, mouseX, mouseY, 150,100)
+
+  if (redx.length < flower2Number){
+    image (roseCut, mouseX-10, mouseY-20, 50, 50)
+
+    print("got red")
+
+  }
+  if (pinkx.length < flower1Number){
+    image (pink, mouseX+20, mouseY-20, 40, 50)
+  }
+
+
+  noCursor()
+
+}
+
+function winGame(){
+  background (random (255), random (255), random (255))
+  fill (255)
+  textSize (40)
+  text('You Win!!',10,100)
+
+  image (pink, windowWidth/2, windowHeight/2, 200, 200)
+
 }
