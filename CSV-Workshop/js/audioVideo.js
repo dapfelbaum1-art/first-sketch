420e420e480046e020b4b902c43a4252d60df3fc
dapfelbaum1-art <dapfelbaum1@gmail.com>
2025-10-21 09:36:38 -0500

    Add files via upload

---
+++ CSV-Workshop/js/audioVideo.js
@@ -0,0 +1,40 @@
+
+
+let modemSound 
+let canvas
+let audioButton 
+
+let modemSlider 
+
+let modemRateSlider
+
+function preload (){
+modemSound = loadSound ('ModemSound.mp3')
+}
+
+function setup (){
+ canvas = createCanvas(windowWidth, windowHeight)
+ canvas.position (0,0)
+ canvas.style ('z-index', '-1')
+
+ audioButton = createButton('Play Modem')
+ audioButton.mousePressed(playAudio)
+
+ modemVolSlider = createSlider(0,3,1,0.01)
+ modemRateSlider = createSlider(0,2,1,0.01)
+ 
+
+}
+
+function playAudio (){
+	if(!modemSound.isPlaying()){
+	modemSound.loop()
+	audioButton.html('Pause Modem')
+}else{
+
+	}
+}
+
+function draw (){
+
+}
